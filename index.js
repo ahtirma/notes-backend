2315b601b7e46ab9963198942b59720b9532a9a1
Amritha Govindarajan <g.amritha@gmail.com>
2022-02-17 14:09:39 +0200

    Part03 - lecture notes - Deploying app to the internet

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
 
 let notes = [
     {
@@ -30,6 +32,7 @@ const requestLogger = (request, response, next) => {
   next();
 }
 
+app.use(cors());
 app.use(express.json());
 app.use(requestLogger);
 
@@ -95,7 +98,7 @@ const unknownEndpoint = (request, response, next) => {
 
 app.use(unknownEndpoint);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
