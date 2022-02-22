b4eb79223f38ad0f86bcaf5c65d0bbca55ffedb5
Amritha Govindarajan <g.amritha@gmail.com>
2022-02-22 16:50:01 +0200

    Part 3: lecture-notes, Validation

--- index.js
+++ index.js
@@ -36,14 +36,8 @@ app.get('/api/notes/:id',(request, response, next) => {
   })
 });
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body;
-
-  if(!body.content) {
-    return response.status(400).json({
-      error: 'content missing',
-    })
-  }
 
   const note = new Note({
     content: body.content,
@@ -51,20 +45,21 @@ app.post('/api/notes', (request, response) => {
     date: new Date(),
   });
 
-  note.save().then(savedNote => {
+  note.save()
+    .then(savedNote => {
     response.json(savedNote);
-  })
+    })
+    .catch(error => next(error));
 });
 
 app.put('/api/notes/:id', (request, response, next) => {
-  const body = request.body;
+  const {content, important} = request.body;
   
-  const note = {
-    content: body.content,
-    important: body.important,
-  }
-
-  Note.findByIdAndUpdate(request.params.id, note, {new: true})
+  Note.findByIdAndUpdate(
+    request.params.id, 
+    {content, important}, 
+    {new: true, runValidators: true, context: 'query'}
+  )
     .then(updatedNote => {
       response.json(updatedNote);
     })
@@ -95,7 +90,9 @@ const errorHandler = (error, request, response, next) => {
   console.error(error.message);
 
   if(error.name === 'CastError') {
-    return response.status(400).send({error: 'malformatted id'})
+    return response.status(400).send({error: 'malformatted id'});
+  } else if(error.name === 'ValidationError') {
+    return response.status(400).json({error: error.message});
   }
   next(error);
 }
