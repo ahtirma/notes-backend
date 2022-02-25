2ee6ce78d16032a7aad33557311a9b1b08f854a9
Amritha Govindarajan <g.amritha@gmail.com>
2022-02-25 16:09:24 +0200

    Part 03: lecture notes - Validating and ESLint

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const requestLogger = (request, response, next) => {
   console.log('Body:', request.body);
   console.log('---');
   next();
-}
+};
 
 app.use(express.static('build'));
 app.use(cors());
@@ -21,8 +21,8 @@ app.use(requestLogger);
 app.get('/api/notes', (request, response) => {
   Note.find({}).then(notes => {
     response.json(notes);
-  })
-})
+  });
+});
 
 app.get('/api/notes/:id',(request, response, next) => {
   Note.findById(request.params.id).then(note => {
@@ -33,7 +33,7 @@ app.get('/api/notes/:id',(request, response, next) => {
     }
   }).catch(error => {
     next(error);
-  })
+  });
 });
 
 app.post('/api/notes', (request, response, next) => {
@@ -47,18 +47,18 @@ app.post('/api/notes', (request, response, next) => {
 
   note.save()
     .then(savedNote => {
-    response.json(savedNote);
+      response.json(savedNote);
     })
     .catch(error => next(error));
 });
 
 app.put('/api/notes/:id', (request, response, next) => {
-  const {content, important} = request.body;
-  
+  const { content, important } = request.body;
+
   Note.findByIdAndUpdate(
-    request.params.id, 
-    {content, important}, 
-    {new: true, runValidators: true, context: 'query'}
+    request.params.id,
+    { content, important },
+    { new: true, runValidators: true, context: 'query' }
   )
     .then(updatedNote => {
       response.json(updatedNote);
@@ -70,19 +70,19 @@ app.put('/api/notes/:id', (request, response, next) => {
 
 app.delete('/api/notes/:id', (request, response, next) => {
   Note.findByIdAndRemove(request.params.id)
-    .then(result => {
+    .then(() => {
       response.status(204).end();
     })
     .catch(error => {
       next(error);
-    })
-})
+    });
+});
 
-const unknownEndpoint = (request, response, next) => {
+const unknownEndpoint = (request, response) => {
   response.status(404).json({
     error: 'unknown endpoint',
-  })
-}
+  });
+};
 
 app.use(unknownEndpoint);
 
@@ -90,16 +90,16 @@ const errorHandler = (error, request, response, next) => {
   console.error(error.message);
 
   if(error.name === 'CastError') {
-    return response.status(400).send({error: 'malformatted id'});
+    return response.status(400).send({ error: 'malformatted id' });
   } else if(error.name === 'ValidationError') {
-    return response.status(400).json({error: error.message});
+    return response.status(400).json({ error: error.message });
   }
   next(error);
-}
+};
 
 app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
--- mongo.js
+++ mongo.js
@@ -1,24 +1,24 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 if (process.argv.length < 3) {
-  console.log('Please provide the password as an argument: node mongo.js <password>')
-  process.exit(1)
+  console.log('Please provide the password as an argument: node mongo.js <password>');
+  process.exit(1);
 }
 
-const password = process.argv[2]
+const password = process.argv[2];
 
 const url =
-    `mongodb+srv://fullstack:${password}@cluster0.isc4j.mongodb.net/noteApp?retryWrites=true&w=majority`
+    `mongodb+srv://fullstack:${password}@cluster0.isc4j.mongodb.net/noteApp?retryWrites=true&w=majority`;
 
-mongoose.connect(url)
+mongoose.connect(url);
 
 const noteSchema = new mongoose.Schema({
   content: String,
   date: Date,
   important: Boolean,
-})
+});
 
-const Note = mongoose.model('Note', noteSchema)
+const Note = mongoose.model('Note', noteSchema);
 
 /* const note = new Note({
   content: 'Callback-functions suck',
@@ -31,9 +31,9 @@ note.save().then(result => {
   mongoose.connection.close()
 })*/
 
-Note.find({important: true}).then(result => {
-    result.forEach(note => {
-        console.log(note);
-    })
-    mongoose.connection.close();
-})
+Note.find({ important: true }).then(result => {
+  result.forEach(note => {
+    console.log(note);
+  });
+  mongoose.connection.close();
+});
